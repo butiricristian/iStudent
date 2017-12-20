f7eaf20752b9c16e9a571ea57833b0315ce7e91e
PatriciaArdelean <PatriciaArdelean>
2017-12-20 19:06:57 +0200

    deleted unecessary packages.

--- news_statistics/src/main/java/ro/ubb/ro/ubb/model/ro/ubb/model/enums/Country.java
+++
@@ -1,6 +0,0 @@
-package ro.ubb.ro.ubb.model.ro.ubb.model.enums;
-
-public enum Country {
-    ROMANIA,GERMANY,UNITED_KINGDOM,FRANCE,USA,HUNGARY,SWITZERLAND,ITALY
-
-}
--- news_statistics/src/main/java/ro/ubb/ro/ubb/model/ro/ubb/model/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package ro.ubb.ro.ubb.model.ro.ubb.model.enums;
-
-public enum Gender {
-    MALE,FEMALE
-}
