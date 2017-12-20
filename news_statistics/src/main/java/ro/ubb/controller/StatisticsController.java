fa7f889243b755a568e44b919e9f0a8331a5a97f
PatriciaArdelean <PatriciaArdelean>
2017-12-20 17:43:00 +0200

    statistics based on age group

---
+++ news_statistics/src/main/java/ro/ubb/controller/StatisticsController.java
@@ -0,0 +1,50 @@
+package ro.ubb.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ro.ubb.dto.NewsAgeStatisticsDto;
+import ro.ubb.service.StatisticsService;
+
+@RestController
+public class StatisticsController {
+
+    @Autowired
+    StatisticsService statisticsService;
+
+    @GetMapping("/statistics/news/youngerThan")
+    public String statisticsFromStudentsYoungerThan() {
+        NewsAgeStatisticsDto statistics = statisticsService.statisticsFromStudentsYoungerThan(30);
+
+        return getJSON(statistics);
+    }
+
+    @GetMapping("/statistics/news/olderThan")
+    public String statisticsFromStudentsOlderThan() {
+        NewsAgeStatisticsDto statistics = statisticsService.statisticsFromStudentsOlderThan(17);
+
+        return getJSON(statistics);
+    }
+
+    @GetMapping("/statistics/news/ageBetween")
+    public String statisticsFromStudentsAgedBetween() {
+        NewsAgeStatisticsDto statistics = statisticsService.statisticsFromStudentsAgedBetween(19, 26);
+
+        return getJSON(statistics);
+    }
+
+
+    private String getJSON(NewsAgeStatisticsDto statistics) {
+        String json = null;
+        try {
+            json = new ObjectMapper().writeValueAsString(statistics);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
+
+
+}
---
+++ news_statistics/src/main/java/ro/ubb/model/User.java
@@ -0,0 +1,26 @@
+package ro.ubb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import ro.ubb.model.enums.Country;
+import ro.ubb.model.enums.Gender;
+
+@Document(collection = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    @Id
+    private Long id;
+    private String name;
+    private int age;
+    private Country country;
+    private Gender gender;
+}
